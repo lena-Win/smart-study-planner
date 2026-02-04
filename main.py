5b19d19365c4a344f8df304b94d7952ce11456df
lena-Win <lenawinczyk@gmail.com>
2026-02-04 12:59:28 +0100

    Add error handling for zero days

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from planner import calculate_daily_study
-result = calculate_daily_study(100, 10)
-print(result)
+print(calculate_daily_study(100, 10))
+print(calculate_daily_study(100, 0))
--- planner.py
+++ planner.py
@@ -1,2 +1,4 @@
 def calculate_daily_study(pages, days):
+    if days == 0:
+        return "Days cannot be zero"
     return pages / days
