5ae1cf1379d8a488e36b406f7640e40568f98da7
lena-Win <lenawinczyk@gmail.com>
2026-02-07 18:20:30 +0100

    Adding manual testing of the planning function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from planner import calculate_daily_study
 print(calculate_daily_study(100, 10))
 print(calculate_daily_study(100, 0))
+print(calculate_daily_study(60, 6))
+print(calculate_daily_study(45, 9))
