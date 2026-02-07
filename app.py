7e7ef7c1b8933a1d6c08f4cde7972a92a25f3358
lena-Win <lenawinczyk@gmail.com>
2026-02-07 17:21:21 +0100

    Connect study planner logic with Flask route

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from planner import calculate_daily_study
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Smart Study planner works"
+    return "Smart Study Planner works"
+@app.route("/plan/<int:pages>/<int:days>")
+def plan(pages, days):
+    result = calculate_daily_study(pages, days)
+    return f"Learn: {result} pages per day"
 if __name__ == "__main__":
     app.run(debug=True)
